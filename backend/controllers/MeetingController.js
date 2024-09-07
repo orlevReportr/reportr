7b3f0811ee0b461739545bb7e7a033c71148c847
slimskhab <slimsskhab@gmail.com>
2024-09-07 14:04:59 +0200

    Add Tailwind CSS and PostCSS configuration files

--- backend/controllers/MeetingController.js
+++ backend/controllers/MeetingController.js
@@ -1,11 +1,11 @@
 const Meeting = require("../models/MeetingModel");
 const Counter = require("../models/CounterModel");
-const axios = require('axios');
+const axios = require("axios");
 const { getSummary, getFormattedSummary } = require("../utils/getSummary");
 
-const createMeeting=async(req,res)=>{
-    try {
-        const{meetingUrl,userId,meetingTitle}=req.body
+const createMeeting = async (req, res) => {
+  try {
+    const { meetingUrl, userId, meetingTitle } = req.body;
 
     const counter = await Counter.findOneAndUpdate(
       { id: "autovalMeeting" },
@@ -13,13 +13,12 @@ const createMeeting=async(req,res)=>{
       { new: true, upsert: true }
     );
 
-   
     const meeting = new Meeting({
       id: counter.seq,
-      status:'Waiting',
+      status: "Waiting",
       userId,
       meetingUrl,
-      meetingTitle
+      meetingTitle,
     });
 
     await meeting.save();
@@ -27,172 +26,176 @@ const createMeeting=async(req,res)=>{
     res.status(201).json({
       status: "success",
       message: "Added Meeting",
-      meeting: meeting
+      meeting: meeting,
     });
-    } catch (error) {
-        return res.status(500).json({ error: 'Server Error!' });
-
-    }
-}
-
-const startRecording = async (req, res) => {
-    try {
-        const {meetingId}=req.body;
-        const meeting=await Meeting.findOne({id:meetingId})
-        if (!meeting) {
-            return res.status(400).json({ error: 'Meeting not found' });
-        }
-        meeting.meetingStartTime = new Date();
-
-        const bot = await recallFetch(meeting.meetingUrl);
-
-        meeting.botId = bot.id;
-        meeting.status='Started';
-        await meeting.save();
-        return res.json({
-            botId: bot.id,
-        });
-    } catch (e) {
-      console.log(e)
-        return res.status(500).json({ error: 'Server Error!' });
-
-    }
-
+  } catch (error) {
+    return res.status(500).json({ error: "Server Error!" });
+  }
 };
 
+const startRecording = async (req, res) => {
+  try {
+    const { meetingId } = req.body;
+    const meeting = await Meeting.findOne({ id: meetingId });
+    if (!meeting) {
+      return res.status(400).json({ error: "Meeting not found" });
+    }
+    meeting.meetingStartTime = new Date();
 
-const recallFetch= async (meetingUrl)=>{
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization':`Token ${process.env.RECALL_API_TOKEN}`
-      };
-console.log(`${process.env.PUBLIC_URL}/transcription`)
-      const reqBody = {
-        bot_name: `${process.env.BOT_NAME}`,
-        meeting_url: meetingUrl,
-        transcription_options: {
-          provider: "default",
-        },
-        real_time_transcription: {
-          destination_url: `${process.env.PUBLIC_URL}/transcription`,
-          partial_results: false,
-        },
-        zoom: {
-          request_recording_permission_on_host_join: true,
-          require_recording_permission: true,
-        },
-        teams:{
-          login_required:true
-        }
-      }
+    const bot = await recallFetch(meeting.meetingUrl);
 
-      
+    meeting.botId = bot.id;
+    meeting.status = "Started";
+    await meeting.save();
+    return res.json({
+      botId: bot.id,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Server Error!" });
+  }
+};
+
+const recallFetch = async (meetingUrl) => {
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Token ${process.env.RECALL_API_TOKEN}`,
+  };
+
+  const reqBody = {
+    bot_name: `${process.env.BOT_NAME}`,
+    meeting_url: meetingUrl,
+    transcription_options: {
+      provider: "default",
+    },
+    real_time_transcription: {
+      destination_url: `${process.env.PUBLIC_URL}/transcription`,
+      partial_results: false,
+    },
+    zoom: {
+      request_recording_permission_on_host_join: true,
+      require_recording_permission: true,
+    },
+    teams: {
+      login_required: true,
+    },
+  };
+
   const res = await axios.post(
     `https://us-west-2.recall.ai/api/v1/bot`,
-    reqBody,{
-        headers
+    reqBody,
+    {
+      headers,
     }
-
-    
   );
-    return res.data;
-}
+  return res.data;
+};
 
 const stopRecording = async (req, res) => {
   try {
     const headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Token ${process.env.RECALL_API_TOKEN}`
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Token ${process.env.RECALL_API_TOKEN}`,
     };
     const { meetingId } = req.body;
-    const meeting = await Meeting.findOne({ id: meetingId })
+    const meeting = await Meeting.findOne({ id: meetingId });
     if (!meeting) {
-      return res.status(400).json({ error: 'Meeting not found' });
+      return res.status(400).json({ error: "Meeting not found" });
     }
 
-    await axios.post(`https://us-west-2.recall.ai/api/v1/bot/${meeting.botId}/leave_call`, {}, { headers });
+    await axios.post(
+      `https://us-west-2.recall.ai/api/v1/bot/${meeting.botId}/leave_call`,
+      {},
+      { headers }
+    );
     meeting.meetingEndTime = new Date();
-    const transcript=formatTranscript(meeting.transcript)
-    meeting.formattedTranscript=transcript;
+    const transcript = formatTranscript(meeting.transcript);
+    meeting.formattedTranscript = transcript;
     const summary = await getSummary(transcript);
-    const formattedSummary = await getFormattedSummary(transcript)
+    const formattedSummary = await getFormattedSummary(transcript);
 
-    meeting.status = 'Stopped';
-    meeting.formattedSummary = formattedSummary
-    meeting.summary = summary; 
+    meeting.status = "Stopped";
+    meeting.formattedSummary = formattedSummary;
+    meeting.summary = summary;
     await meeting.save();
 
-    return res.status(200).json({ message: "Recording Stopped ", summary: summary }); // Return the summary
+    return res
+      .status(200)
+      .json({ message: "Recording Stopped ", summary: summary }); // Return the summary
   } catch (e) {
-    console.log(e)
+    console.log(e);
     return res.status(500).json({ message: "Server Error" });
   }
 };
 
 function formatTranscript(transcript) {
-  var formattedTranscript="";
+  var formattedTranscript = "";
   let currentSpeaker = null;
-  let currentSentence = '';
+  let currentSentence = "";
 
-  transcript.forEach(sentence => {
+  transcript.forEach((sentence) => {
     if (sentence.speaker !== currentSpeaker) {
-
       if (currentSpeaker !== null) {
-        formattedTranscript=formattedTranscript + `${currentSpeaker}: ${currentSentence.trim()}`;
-        currentSentence = ''; 
+        formattedTranscript =
+          formattedTranscript + `${currentSpeaker}: ${currentSentence.trim()}`;
+        currentSentence = "";
       }
       currentSpeaker = sentence.speaker;
     }
 
-    currentSentence += sentence.words.map(word => word.text).join(" ");
+    currentSentence += sentence.words.map((word) => word.text).join(" ");
   });
 
   if (currentSpeaker !== null) {
-    formattedTranscript=formattedTranscript + `${currentSpeaker}: ${currentSentence.trim()}`;
+    formattedTranscript =
+      formattedTranscript + `${currentSpeaker}: ${currentSentence.trim()}`;
   }
-console.log(formattedTranscript)
+  console.log(formattedTranscript);
   return formattedTranscript;
 }
 
+const getUserMeetings = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const meetings = await Meeting.find({ userId });
 
-const getUserMeetings=async (req,res)=>{
-  try{
-      const {userId}=req.body
-      const meetings=await Meeting.find({userId})
-
-      return res.status(200).json({
-          status: "success",
-          message: "Meetings retrieved",
-          meetings
-        }); 
-  }catch(e){
-      console.error(e);
+    return res.status(200).json({
+      status: "success",
+      message: "Meetings retrieved",
+      meetings,
+    });
+  } catch (e) {
+    console.error(e);
     res.status(500).json({
-      message: "Server Error!"
+      message: "Server Error!",
     });
   }
-}
+};
 
-const getOneMeeting=async (req,res)=>{
-  try{
-      const {meetingId}=req.body;
-      const meeting=await Meeting.findOne({_id:meetingId})
+const getOneMeeting = async (req, res) => {
+  try {
+    const { meetingId } = req.body;
+    const meeting = await Meeting.findOne({ _id: meetingId });
 
-      return res.status(200).json({
-          status: "success",
-          message: "Meeting retrieved",
-          meeting
-        }); 
-  }catch(e){
-      console.error(e);
+    return res.status(200).json({
+      status: "success",
+      message: "Meeting retrieved",
+      meeting,
+    });
+  } catch (e) {
+    console.error(e);
     res.status(500).json({
-      message: "Server Error!"
+      message: "Server Error!",
     });
   }
-}
+};
 
 module.exports = {
-  startRecording,createMeeting,stopRecording,getUserMeetings,getOneMeeting
+  startRecording,
+  createMeeting,
+  stopRecording,
+  getUserMeetings,
+  getOneMeeting,
 };
