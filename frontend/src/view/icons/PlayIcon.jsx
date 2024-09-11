ad6914328a6c9989fbe3a1e24934f41de6509736
slimskhab <slimsskhab@gmail.com>
2024-09-11 05:09:41 +0200

    Add default icon component

---
+++ frontend/src/view/icons/PlayIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function PlayIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="16"
+      viewBox="0 0 18 21"
+      fill="none"
+    >
+      <path
+        d="M17.3677 9.63031C18.0429 10.0135 18.0429 10.9865 17.3677 11.3697L1.89363 20.1523C1.22699 20.5306 0.400024 20.0491 0.400024 19.2826L0.400024 1.71741C0.400024 0.950878 1.22699 0.469359 1.89363 0.847722L17.3677 9.63031Z"
+        fill="white"
+      ></path>
+    </svg>
+  );
+}
+
+export default PlayIcon;
